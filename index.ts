5db524a06f64e5faf485ddf4dc6a2bf9df05936f
medinsky <webmedinsky@gmail.com>
2018-12-11 00:44:29 +0300

    init

---
+++ index.ts
@@ -0,0 +1,27 @@
+function pow(a: number, n: number) {
+  let res: number = 1;
+  while (n !== 0) {
+    if (isOdd(n)) {
+      res = multiply(a, res);
+      n = n - 1;
+    } else {
+      a = multiply(a, a);
+      n = devideEven(n);
+    }
+  }
+  return res;
+}
+
+function isOdd(n: number): boolean {
+  return n % 2 === 1;
+}
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function devideEven(n: number): number {
+  return n / 2;
+}
+
+console.log(pow(2, 3));
