15d3ae2fd5b59abee5123c12e283b78ef896f2f8
Uladzimir Miadzinski <Uladzimir_Miadzinski@epam.com>
2018-12-11 12:24:32 +0300

    shorter code

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 var isOdd = function (n) { return n % 2 === 1; };
 var multiply = function (a, b) { return a * b; };
 var devideEven = function (n) { return n / 2; };
-function pow(a, n) {
-    console.log(new Array(n));
-    var result = [1, 2, 3].reduce(function (result, item, index) {
-        console.log("a: " + index);
-        console.log("result: " + result);
-        return multiply(result, a);
-    }, 1);
-    console.log(result);
-}
-function calc(a, n, result) {
+var square = function (n) { return multiply(n, n); };
+var calc = function (a, n, result) {
     return isOdd(n)
         ? {
             result: multiply(a, result),
@@ -20,5 +12,44 @@ function calc(a, n, result) {
             a: multiply(a, a),
             n: devideEven(n)
         };
+};
+var iterations = 0;
+function pow(a, n) {
+    var result = 1;
+    while (n !== 0) {
+        iterations++;
+        if (isOdd(n)) {
+            result = multiply(a, result);
+            n = n - 1;
+        }
+        else {
+            a = multiply(a, a);
+            n = devideEven(n);
+        }
+    }
+    return result;
 }
-console.log(pow(2, 3));
+function spread(n, result) {
+    if (result === void 0) { result = []; }
+    if (n === 1) {
+        console.log(result);
+        return result;
+    }
+    else if (isOdd(n)) {
+        return spread(n - 1, result);
+    }
+    else {
+        result.push([2]);
+        return spread(devideEven(n), result);
+    }
+}
+var arrPow = function (n, exp) { return isMultiplierExists(exp) ? (Math.pow(n, exp[0])) * n : Math.pow(n, exp[0]); };
+var isMultiplierExists = function (exp) { console.log(exp.length > 1); return exp.length > 1; };
+var powAsSpread = function (n, exp) { return spread(exp).reduceRight(function (result, item) {
+    console.log("result: " + result);
+    console.log("item: " + item);
+    var powed = arrPow(result, item);
+    console.log("powed: " + powed);
+    return powed;
+}, n); };
+console.log(powAsSpread(2, 9));
--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ const multiply = (a: number, b: number): number => a * b;
 
 const devideEven = (n: number): number => n / 2;
 
+const square = (n: number): number => multiply(n, n);
+
 const calc = (a: number, n: number, result: number) =>
   isOdd(n)
     ? {
@@ -15,9 +17,12 @@ const calc = (a: number, n: number, result: number) =>
       n: devideEven(n)
     };
 
+let iterations = 0;
+
 function pow(a: number, n: number) {
   let result: number = 1;
   while (n !== 0) {
+    iterations++;
     if (isOdd(n)) {
       result = multiply(a, result);
       n = n - 1;
@@ -29,4 +34,28 @@ function pow(a: number, n: number) {
   return result;
 }
 
-console.log(pow(2, 3));
+function spread(n: number, result: number[][] = []) {
+  if (n === 1) {
+    console.log(result);
+    return result;
+  } else if (isOdd(n)) {
+    return spread(n - 1, result);
+  } else {
+    result.push([2]);
+    return spread(devideEven(n), result);
+  }
+}
+
+const arrPow = (n: number, exp: number[]) => isMultiplierExists(exp) ? (n ** exp[0]) * n : n ** exp[0];
+
+const isMultiplierExists = (exp: number[]) => { console.log(exp.length > 1); return exp.length > 1 };
+
+const powAsSpread = (n: number, exp: number) => spread(exp).reduceRight((result, item) => {
+  console.log(`result: ${result}`);
+  console.log(`item: ${item}`);
+  const powed = arrPow(result, item);
+  console.log(`powed: ${powed}`);
+  return powed;
+}, n);
+
+console.log(powAsSpread(2, 4));
