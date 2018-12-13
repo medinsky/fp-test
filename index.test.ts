4909e295138f5b4719d6d7067e668be76d300487
Uladzimir Miadzinski <Uladzimir_Miadzinski@epam.com>
2018-12-13 15:20:25 +0300

    tested multiply

--- index.test.ts
+++ index.test.ts
@@ -1,5 +1,6 @@
 import { subtractByModule } from "./subtract";
 import { addByModule } from "./add";
+import { multiply } from "./multiply";
 
 it('should subtract values', () => {
   for (let i = 0; i < 1000; i++) {
@@ -13,6 +14,14 @@ it('should add values', () => {
   for (let i = 0; i < 1000; i++) {
     for (let j = 1000; j >= 0; j--) {
       expect(addByModule(i.toString(), j.toString())).toBe((i + j).toString());
+    }
+  }
+});
+
+it('should multiply values', () => {
+  for (let i = 0; i < 1000; i++) {
+    for (let j = 1000; j >= 0; j--) {
+      expect(multiply(i.toString(), j.toString())).toBe((i * j).toString());
     }
   }
 });
