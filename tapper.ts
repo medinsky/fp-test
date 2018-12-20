511fad05ba69c76d60500664091bd9620c0b5fbb
medinsky <webmedinsky@gmail.com>
2018-12-20 16:09:24 +0300

    rewrite tapper

--- tapper.ts
+++ tapper.ts
@@ -2,10 +2,14 @@ import {mod} from "./actions/mod";
 import {divide} from "./actions/divide";
 import {multiply} from "./actions/multiply";
 import {pow} from "./actions/pow";
-import {addByModule} from "./actions/add";
 
 export const tapper = (x: string, y: string): string => {
-  return mod(multiply(divide(y, '17'), divide('1', pow('2', addByModule(multiply('17', x), mod(y, '17'))))), '2');
+  const modOfY: string = mod(y, '17');
+  const powForTwo: string = multiply(multiply('17', x), modOfY);
+  const twoInPowForTwo: string = pow('2', powForTwo);
+  const denominator: string = multiply('17', twoInPowForTwo);
+  const lastModFirstArg: string = divide(y, denominator);
+  return mod(lastModFirstArg, '2');
 };
 
-export const halfCompareTapper = (x: string, y: string) => 0.5 >= parseInt(tapper(x, y));
+export const halfCompareTapper = (x: string, y: string): boolean => 0.5 < parseInt(tapper(x, y));
